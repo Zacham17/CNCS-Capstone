b69671af243c64df7123465a040757e0e94b49b6
Zachary Morris <70924115+Zacham17@users.noreply.github.com>
2024-04-20 16:33:55 -0400

    Update urls.py

--- Implementation Sprint 4/Sprint4Week2/WebsiteFiles(PostParser)/urls.py
+++ Implementation Sprint 4/Sprint4Week2/WebsiteFiles(PostParser)/urls.py
@@ -1,7 +1,1 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('ticket/<int:ticket_id>', views.ticket_by_id, name='ticket_by_id')
-]
+Placeholder
