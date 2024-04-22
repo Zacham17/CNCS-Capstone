b442efeae07d93947053349b9f9388650ff17c04
Zachary Morris <70924115+Zacham17@users.noreply.github.com>
2024-04-22 00:29:08 -0400

    Delete Final Files/ticketSite directory

--- Final Files/ticketSite/ticketApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('ticket/<int:ticket_id>', views.ticket_by_id, name='ticket_by_id'),
-    path('refresh_output/', views.refresh_output, name='refresh_output'),
-]
